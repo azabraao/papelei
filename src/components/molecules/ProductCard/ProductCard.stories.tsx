8b19461a1c5fddfa113c53cfbe3747402826c765
Abraão Azevedo <az.abraao@gmail.com>
2022-06-13 14:42:24 -0300

    move ProductCard to molecules

--- src/components/molecules/ProductCard/ProductCard.stories.tsx
+++ src/components/molecules/ProductCard/ProductCard.stories.tsx
@@ -12,9 +12,7 @@ export default {
 } as ComponentMeta<typeof ProductCard>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof ProductCard> = (args) => (
-  <ProductCard {...args} />
-);
+const Template: ComponentStory<typeof ProductCard> = () => <ProductCard />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,6 +1,7 @@
 export { default as Navbar } from "./Navbar";
 export { default as AppCopyTitle } from "./AppCopyTitle";
 export { default as PaymentMethodSelector } from "./PaymentMethodSelector";
+export { default as ProductCard } from "./ProductCard";
 export { default as TextInput } from "./Form/TextInput";
 export { default as TextArea } from "./Form/TextArea";
 export { default as MoneyInput } from "./Form/MoneyInput";
