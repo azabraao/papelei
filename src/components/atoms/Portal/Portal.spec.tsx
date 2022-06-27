7a8b5ad348e988e22baccfd2913c17507082c693
Abraão Azevedo <az.abraao@gmail.com>
2022-06-27 17:09:48 -0300

    small updates to Portal component

--- src/components/atoms/Portal/Portal.spec.tsx
+++ src/components/atoms/Portal/Portal.spec.tsx
@@ -4,10 +4,10 @@ import Portal from "./index";
 
 describe("Portal", () => {
   it("should render fixed to the viewport", () => {
-    render(<Portal />);
+    render(<Portal isActive={true} />);
 
     expect(screen.getByTestId("portal")).toHaveClass(
-      "bg-black-transparent fixed top-0 right-0 bottom-0 left-0"
+      "bg-black-transparent absolute top-0 right-0 bottom-0 left-0"
     );
   });
 });
--- src/components/atoms/Portal/index.tsx
+++ src/components/atoms/Portal/index.tsx
@@ -1,15 +1,31 @@
+import clsx from "clsx";
 import { memo } from "react";
 
 interface PortalProps {
+  isActive: boolean;
   onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  transitionDuration?: number;
 }
 
-const Portal = ({ onClick }: PortalProps) => (
+const Portal = ({ transitionDuration, isActive, onClick }: PortalProps) => (
   <div
     onClick={onClick}
     data-testid="portal"
-    className="bg-black-transparent fixed top-0 right-0 bottom-0 left-0 z-10"
+    className={clsx(
+      "bg-black-transparent absolute top-0 right-0 bottom-0 left-0 transition-opacity z-10",
+      {
+        "duration-1000": transitionDuration === 1000,
+        "duration-500": transitionDuration === 500,
+        "duration-300": transitionDuration === 300,
+        "opacity-0 pointer-events-none": !isActive,
+        "opacity-100 pointer-events-auto": isActive,
+      }
+    )}
   />
 );
+
+Portal.defaultProps = {
+  transitionDuration: 300,
+};
 
 export default memo(Portal);
