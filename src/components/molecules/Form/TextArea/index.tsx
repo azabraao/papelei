f6a63820cdb36dec316cb79dc7a72a319c1f1954
Abraão Azevedo <az.abraao@gmail.com>
2022-06-27 21:17:29 -0300

    forward TextArea component ref

--- src/components/molecules/Form/TextArea/index.tsx
+++ src/components/molecules/Form/TextArea/index.tsx
@@ -1,4 +1,5 @@
-import { memo } from "react";
+/* eslint-disable react/display-name */
+import { forwardRef, memo } from "react";
 import clsx from "clsx";
 import TextareaAutosize from "react-textarea-autosize";
 import { CheckedIcon, XIcon } from "components/atoms";
@@ -8,45 +9,45 @@ interface TextAreaProps extends InputProps {
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-const TextArea = ({
-  name,
-  error,
-  isSuccess,
-  placeholder,
-  onChange,
-}: TextAreaProps) => {
-  return (
-    <div
-      className={clsx(
-        "flex items-start px-4 py-2 justify-between bg-white rounded-lg border hover:shadow-on-hover-shadow placeholder-black-40 w-full overflow-hidden",
-        error &&
-          "border-danger focus-within:border-danger focus-within:shadow-none",
-        isSuccess &&
-          "border-success focus-within:border-success focus-within:shadow-none",
-        !error &&
-          !isSuccess &&
-          "border-black-20 focus-within:shadow-focus-shadow-info focus-within:border-info-light"
-      )}
-      data-testid={clsx(
-        error && "error-state",
-        isSuccess && "success-state",
-        !error && !isSuccess && "input"
-      )}
-    >
-      <TextareaAutosize
-        className="outline-none w-full resize-none text-black-70 h-auto "
-        placeholder={placeholder}
-        onChange={onChange}
-        minRows={3}
-        name={name}
-        id={name}
-      />
+const TextArea = forwardRef(
+  (
+    { name, error, isSuccess, placeholder, onChange }: TextAreaProps,
+    ref: React.Ref<HTMLTextAreaElement>
+  ) => {
+    return (
+      <div
+        className={clsx(
+          "flex items-start px-4 py-2 justify-between bg-white rounded-lg border hover:shadow-on-hover-shadow placeholder-black-40 w-full overflow-hidden",
+          error &&
+            "border-danger focus-within:border-danger focus-within:shadow-none",
+          isSuccess &&
+            "border-success focus-within:border-success focus-within:shadow-none",
+          !error &&
+            !isSuccess &&
+            "border-black-20 focus-within:shadow-focus-shadow-info focus-within:border-info-light"
+        )}
+        data-testid={clsx(
+          error && "error-state",
+          isSuccess && "success-state",
+          !error && !isSuccess && "input"
+        )}
+      >
+        <TextareaAutosize
+          className="outline-none w-full resize-none text-black-70 h-auto "
+          placeholder={placeholder}
+          onChange={onChange}
+          minRows={3}
+          name={name}
+          id={name}
+          ref={ref}
+        />
 
-      {error && <XIcon theme="danger" />}
-      {isSuccess && <CheckedIcon theme="success" />}
-    </div>
-  );
-};
+        {error && <XIcon theme="danger" />}
+        {isSuccess && <CheckedIcon theme="success" />}
+      </div>
+    );
+  }
+);
 
 TextArea.defaultProps = {
   error: "",
