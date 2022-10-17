691978690a0f22a518ee687fbee3f55388defba4
Abraão Azevedo <az.abraao@gmail.com>
2022-10-17 19:33:46 -0300

    style: set limit to navbar

--- src/components/molecules/BottomBar/index.tsx
+++ src/components/molecules/BottomBar/index.tsx
@@ -1,26 +1,34 @@
 import { memo } from "react";
-import { ActiveLink, ConfigIcon, HomeIcon, UserIcon } from "components/atoms";
+import {
+  ActiveLink,
+  ConfigIcon,
+  Container,
+  HomeIcon,
+  UserIcon,
+} from "components/atoms";
 
 const BottomBar = () => {
   return (
-    <div className="fixed bottom-0 right-0 left-0 mx-auto p-4 w-full">
-      <div className="flex bg-white text-gray-700 justify-between items-center shadow-elevation-1 px-5 rounded-full">
-        <ActiveLink activeClassName="text-success" href="/">
-          <div className="p-3 flex justify-center items-center">
-            <HomeIcon />
-          </div>
-        </ActiveLink>
-        <ActiveLink activeClassName="text-success" href="/profile">
-          <div className="p-3 flex justify-center items-center">
-            <UserIcon />
-          </div>
-        </ActiveLink>
-        <ActiveLink activeClassName="text-success" href="/config">
-          <div className="p-3 flex justify-center items-center">
-            <ConfigIcon />
-          </div>
-        </ActiveLink>
-      </div>
+    <div className="fixed bottom-0 right-0 left-0 mx-auto py-4 w-full">
+      <Container>
+        <div className="flex bg-white text-gray-700 justify-between items-center shadow-elevation-1 px-5 rounded-full">
+          <ActiveLink activeClassName="text-success" href="/">
+            <div className="p-3 flex justify-center items-center">
+              <HomeIcon />
+            </div>
+          </ActiveLink>
+          <ActiveLink activeClassName="text-success" href="/profile">
+            <div className="p-3 flex justify-center items-center">
+              <UserIcon />
+            </div>
+          </ActiveLink>
+          <ActiveLink activeClassName="text-success" href="/config">
+            <div className="p-3 flex justify-center items-center">
+              <ConfigIcon />
+            </div>
+          </ActiveLink>
+        </div>
+      </Container>
     </div>
   );
 };
