324eeaab44679cdd7a7e2e57bf224e95fc00b007
Abraão Azevedo <az.abraao@gmail.com>
2022-10-20 19:42:50 -0300

    add route to list products with pagination

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,29 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function addProduct(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET")
+    return res.status(500).json({ message: "wrong method" });
+
+  const { businessID, skip } = await req.body;
+
+  try {
+    const product = await prisma.product.findMany({
+      skip,
+      take: 3,
+      where: {
+        businessID,
+      },
+    });
+
+    res.json({ product });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+}
+
+export default withIronSessionApiRoute(addProduct, sessionOptions);
