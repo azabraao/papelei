95853df34a9e55d95e34f9aca9e96bbbb5e707b2
Abraão Azevedo <az.abraao@gmail.com>
2022-11-25 14:05:01 -0300

    guarantee login button to be visible

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -34,7 +34,7 @@ export default function Auth() {
             <p className="text-lg ">Pra começar a vender!</p>
           </div>
 
-          <main className="flex flex-col gap-4">
+          <main className="flex flex-col gap-4 pb-10">
             <GoogleButton handleLogin={handleLogin} />
             {/* <FacebookButton handleLogin={handleLogin} /> */}
           </main>
